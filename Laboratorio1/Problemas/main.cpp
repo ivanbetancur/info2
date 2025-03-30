9837cb3893444fd5136e4cabd70c625e7149d902
ivanbetancur <i.betancur@udea.edu.co>
2025-03-30 06:52:36 -0500

    Signed-off-by: ivanbetancur <i.betancur@udea.edu.co>

--- Laboratorio1/Problemas/main.cpp
+++ Laboratorio1/Problemas/main.cpp
@@ -10,12 +10,13 @@ using namespace std;
 void Ejercicio1() {
     /*Ejercicio 1. Escriba un programa que pida dos números A y B e imprima en pantalla el residuo
             de la división A/B.*/
-    int a, b;
+    int a, b, c;
     std::cout << "Ingrese numero A: ";
     std::cin >> a;
     std::cout << "Ingrese numero B: ";
     std::cin >> b;
-    std::cout << "El residuo de la division " << a << "/" << b << " es: " << (a % b) << std::endl;
+    c= a%b;
+    std::cout << "El residuo de la division " << a << "/" << b << " es: " << c << std::endl;
 }
 
 void Ejercicio3() {
@@ -31,7 +32,8 @@ void Ejercicio3() {
         std::cout << "El numero mayor es " << a << std::endl;
     } else if (b > a) {
         std::cout << "El numero mayor es " << b << std::endl;
-    } else {
+    }
+    else {
         std::cout << "Ambos numeros son iguales." << std::endl;
     }
 
@@ -42,14 +44,15 @@ void Ejercicio5() {
     /*Ejercicio 5. Escriba un programa que pida dos números A y B e imprima en pantalla la división
 A/B con redondeo.*/
 
-    double a, b;
+    double a, b,c;
     std::cout << "Ingrese A: ";
     std::cin >> a;
     std::cout << "Ingrese B: ";
     std::cin >> b;
 
     if (b != 0) {
-        std::cout << "El resultado de la division redondeada es: " << round(a / b) << std::endl;
+        c=round(a/b);
+        std::cout << "El resultado de la division redondeada es: " << c << std::endl;
     } else {
         std::cout << "Error: division por cero no permitida" << std::endl;
     }
@@ -61,19 +64,23 @@ void Ejercicio7() {
     /*Ejercicio 7.Escriba un programa que pida un número N e imprima en pantalla la suma de todos
 los números entre 0 y N (incluyéndose el mismo).*/
 
-    int n, suma = 0;
-    std::cout << "Ingrese N: ";
+    int n;
+    int suma = 0;
+
+    std::cout << "digite un numero: ";
     std::cin >> n;
 
     std::cout << "La sumatoria desde 0 hasta " << n << " es: ";
-    for (int i = 1; i <= n; i++) {
-        suma += i;
+    for (int i = 1; i <= n; i++)
+    {
+        suma = suma + i;
         std::cout << i;
-        if (i < n) {
-            std::cout << "+";
+        if (i < n)
+        {
+            std::cout << " + ";
         }
     }
-    std::cout << "=" << suma << std::endl;
+    std::cout << " = " << suma << std::endl;
 
 }
 
@@ -81,16 +88,16 @@ void Ejercicio9() {
 
     /*Ejercicio 9. Escriba un programa que pida un número N e imprima el perímetro y área de un
 círculo con radio N. Nota: use 3.1416 como una aproximación de pi. A = π r²  Perímetro = 2 · r · π */
-    #define PI 3.1416
+    double PI= 3.1416;
     double n;
-    std::cout << "Ingrese N: ";
+    std::cout << "Ingrese el radio del circulo: ";
     std::cin >> n;
 
     double perimetro = 2 * PI * n;
     double area = PI * n * n;
 
-    std::cout << "Perimetro: " << perimetro << std::endl;
-    std::cout << "Area: " << area << std::endl;
+    std::cout << "el perimetro es: " << perimetro << std::endl;
+    std::cout << "el area es: " << area << std::endl;
 
 }
 
@@ -98,31 +105,38 @@ void Ejercicio11() {
 
 /*Ejercicio 11. Escriba un programa que pida un número N e imprima en pantalla su tabla de multiplicar
 hasta 10xN. */
-    int n;
+    int n, mult;
     std::cout << "Ingrese N: ";
     std::cin >> n;
-
-    for (int i = 1; i < 11; i++) {
-        std::cout << i << "x" << n << "=" << i * n << std::endl;
+    std::cout << "la tabla del "<< n << " es:"<< std::endl;
+    for (int i = 1; i < 11; i++)
+    {
+        mult=i*n;
+        std::cout << i << "x" << n << "=" << mult << std::endl;
     }
 
 }
+
+
 
 void Ejercicio13() {
 
     /*Ejercicio 13. Escriba un programa que pida un número N e imprima todos los divisores de N. */
     int n;
-    std::cout << "Ingrese N: ";
+    std::cout << "Ingrese un numero ";
     std::cin >> n;
 
     std::cout << "Los divisores de " << n << " son:" << std::endl;
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            std::cout << i << std::endl;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            std::cout << i << ",";
         }
     }
 
 }
+
 
 void Ejercicio15() {
 
@@ -629,6 +643,6 @@ con el valor del número anterior: 1, 1+2=3, 3+3=6, 6+4=10, 10+5=15, 15+6=21, 21
 
 int main()
 {
-    Ejercicio7();
+    Ejercicio13();
 
 }
